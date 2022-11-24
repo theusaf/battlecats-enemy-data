e3b1dc8e9741d32aca76c7a7d2c94ab1dfa419dd
theusaf <theusafyt@gmail.com>
2022-11-24 10:58:27 -0800

    feat: parse wiki data

--- job/wiki-database.ts
+++ job/wiki-database.ts
@@ -47,3 +47,61 @@ export async function getEnemyPage(title: string): Promise<string> {
   const response = await bot.read(title);
   return (getFirstValue(response.query.pages) as any).revisions[0]["*"];
 }
+
+export interface Appearance {
+  section: string;
+  stageWikiTitle: string;
+  stageTitle: string;
+}
+
+export interface WikiEnemyData {
+  name: string;
+  image: string;
+  latestAppearance: Appearance;
+  firstAppearances: Appearance[];
+}
+
+export async function parseEnemyPage(page: string, expectedId: string) {
+  const linkRegex = new RegExp(
+    `==Reference==[\\s\\n]*\\*\\s*https://battlecats-db\\.com/enemy/${expectedId}\\.html`
+  );
+  if (!linkRegex.test(page)) return null;
+  const [, name] = page.match(/\|name\s*=\s*([^|]+)/),
+    [, image] = page.match(/\|image\s*=\s*([^|]+)/),
+    [, firstAppearanceValue] = page.match(
+      /\|first appearance\s*=\s*('''\w+''':\s*\[\[.*?\]\]\s*(<br>[\s\n]*)?)*/
+    ),
+    [, latestApperanceValue] = page.match(
+      /\|latest appearance\s*=\s*('''\w+''':\s*\[\[.*?\]\]\s*(<br>[\s\n]*)?)*/
+    ),
+    firstAppearances = firstAppearanceValue
+      .match(/('''\w+''':\s*\[\[.*?]])/g)
+      .map((match) => {
+        const [, section, stageWikiTitle, stageTitle] = match.match(
+          /'''(\w+)''':\s*\[\[(.*?)\|(.*?)]]/
+        );
+        return {
+          section,
+          stageWikiTitle,
+          stageTitle,
+        };
+      }),
+    latestAppearances = latestApperanceValue
+      .match(/('''\w+''':\s*\[\[.*?]])/g)
+      .map((match) => {
+        const [, section, stageWikiTitle, stageTitle] = match.match(
+          /'''(\w+)''':\s*\[\[(.*?)\|(.*?)]]/
+        );
+        return {
+          section,
+          stageWikiTitle,
+          stageTitle,
+        };
+      });
+  return {
+    name,
+    image,
+    firstAppearances,
+    latestAppearance: latestAppearances[0],
+  };
+}
