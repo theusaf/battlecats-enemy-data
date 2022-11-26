8f8afe387037fbfe9ea8103b2cb7882e3e6cb901
theusaf <theusafyt@gmail.com>
2022-11-26 13:01:57 -0800

    chore: finish human review 1

--- job/humanReview.ts
+++ job/humanReview.ts
@@ -5,6 +5,8 @@ import {
   needsHumanReview,
   writeEnemyData,
   writeNeedsHumanReview,
+  writeScrapedIDs,
+  writeScrapedWiki,
 } from "./data.js";
 import { getEnemyPage, parseEnemyPage } from "./wiki-database.js";
 
@@ -16,31 +18,40 @@ const rl = readline.createInterface({
 const initialLength = needsHumanReview.length;
 
 for (let i = needsHumanReview.length - 1; i >= 0; i--) {
-  const id = needsHumanReview.pop(),
-    basicEnemyData = await getEnemyData(id),
-    wikiURL = await rl.question(
-      `[${initialLength - i}/${initialLength}] Enter the wiki URL for ${
-        basicEnemyData.name
-      } ($${basicEnemyData.id}):\n`
-    );
-  if (!wikiURL.trim()) {
-    needsHumanReview.splice(0, 0, id); // Put it back at the beginning
-    continue;
-  }
-  const title = wikiURL.match(/\/wiki\/(.*)$/)[1],
-    page = await getEnemyPage(title),
-    parsed = parseEnemyPage(page, id);
-  if (!parsed) {
-    console.log("Match not found. Try again.");
+  const id = needsHumanReview.pop();
+  try {
+    const basicEnemyData = await getEnemyData(id),
+      wikiURL = await rl.question(
+        `[${initialLength - i}/${initialLength}] Enter the wiki URL for ${
+          basicEnemyData.name
+        } (${basicEnemyData.id}):\n`
+      );
+    if (!wikiURL.trim()) {
+      needsHumanReview.splice(0, 0, id); // Put it back at the beginning
+      continue;
+    }
+    const title = wikiURL.match(/\/wiki\/(.*)$/)[1],
+      page = await getEnemyPage(title),
+      parsed = parseEnemyPage(page, id);
+    if (!parsed) {
+      console.log("Match not found. Try again.");
+      needsHumanReview.push(id);
+      i++;
+    } else {
+      addEnemy({
+        ...basicEnemyData,
+        ...parsed,
+        wikiTitle: title,
+      });
+    }
+  } catch (e) {
+    console.log("An error occured. Try again.");
     needsHumanReview.push(id);
-  } else {
-    addEnemy({
-      ...basicEnemyData,
-      ...parsed,
-      wikiTitle: title,
-    });
+    i++;
   }
 }
 await writeEnemyData();
 await writeNeedsHumanReview();
+await writeScrapedIDs();
+await writeScrapedWiki();
 rl.close();
